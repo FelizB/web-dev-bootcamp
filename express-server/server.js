47651d55cb8cf3a9856424758687c5c3752a0626
Felix <bosirefelix97@gmail.com>
2023-04-16 14:18:27 +0300

    create express server

--- express-server/server.js
+++ express-server/server.js
@@ -1,3 +1,9 @@
 const express = require ("express");
 const app = express();
-app.listen(3000);
+app.get("/", function(req,res){
+    response.send("Hello")
+});
+
+app.listen(3000, function(){
+    console.log("server has started on port 3000");
+});
