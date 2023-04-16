fb298ef8cc2ac6541c31b348d86e12799a803319
Felix <bosirefelix97@gmail.com>
2023-04-16 15:39:18 +0300

    BMI challenge

---
+++ BMICalc/bmiCalculator.js
@@ -0,0 +1,19 @@
+const express = require ("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/bmi.html");
+});
+
+app.post("/", function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var BMI = weight/(height*height);
+    res.send("The BMI value is "+BMI);
+});
+
+app.listen(3000, function(){
+    console.log("server started and listening to port 3000");
+});
