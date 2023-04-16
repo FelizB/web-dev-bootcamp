5b9147277815f24e17a216e2967eec3adf074319
Felix <bosirefelix97@gmail.com>
2023-04-16 14:33:10 +0300

    more express

--- express-server/server.js
+++ express-server/server.js
@@ -1,7 +1,15 @@
 const express = require ("express");
 const app = express();
 app.get("/", function(req,res){
-    response.send("Hello")
+    res.send("Hello")
+});
+
+app.get("/contact", function(req,res){
+    res.send("Cotact me on felix.buzz@gmail.com")
+});
+
+app.get("/about", function(req,res){
+    res.send("My name is Felix and I love coding")
 });
 
 app.listen(3000, function(){
