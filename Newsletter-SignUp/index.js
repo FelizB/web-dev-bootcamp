ade64afb8c3e520e453df0957bb17c67fd677063
Felix <bosirefelix97@gmail.com>
2023-04-30 15:01:44 +0300

    test

--- Newsletter-SignUp/index.js
+++ Newsletter-SignUp/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
@@ -10,9 +13,53 @@ app.get("/", function(req, res){
 
 });
 
+app.post("/failure", function(req,res){
+    res.redirect("/")
+});
 app.post("/",function(req,res){
+    const firstName = req.body.Fname
+    const SecName = req.body.Lname
+    const Email = req.body.email
+    var data= {
+        members:[
+            {
+            email_address:Email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME: firstName,
+                LNAME: SecName
+            }
+            }
+        ]
+    };
+    var jsonData= JSON.stringify(data)
+    const url ="https://us21.api.mailchip.com/3.0/lists/a0e1d58629";
+    const options ={
+        method:"POST",
+        auth:"crazychizas@gmail.com:f535a07c2fb7b172f64768dc27f67c32-us21"
+    } 
+    const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data",function(data){
+            console.log(jsonData) 
+        });
+        
+    });
+    request.write(jsonData);
+    request.end();
 
 });
 app.listen(3000, function(){
     console.log("listening to port 3000");
 });
+//API KEY
+//f535a07c2fb7b172f64768dc27f67c32-us21
+//list ID
+//a0e1d58629
--- WeatherProject/index.js
+++ WeatherProject/index.js
@@ -11,7 +11,7 @@ app.get("/",function(req, res){
 });
 
 app.post("/", function(req, res){
-    const query= req.body.cityName
+    /*const query= req.body.cityName
     const key = "8b8059e1f19e38b5843f3660a47265b5"
     const unit= "metric"
     const url ="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+key+"&unit="+unit
@@ -26,8 +26,26 @@ app.post("/", function(req, res){
             res.write("<h1>The temperature in "+query+" is " + temp +" degrees Celcius.</h1>")
             res.write("<img src="+imgURL+">")
             res.send()
-        })
+        });
+    });*/
+    const la=req.body.cityName
+    const key = "8b8059e1f19e38b5843f3660a47265b5"
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat="+la+"&lon=10.99&appid="+ key+"&units=metric"
+    https.get(url,function(response){
+        response.on("data",function(data){
+            const date = JSON.parse(data)
+            const temp =date.main.temp
+            const des = date.weather[0].description
+            const icon = date.weather[0].icon
+            const imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+            res.write("<p>The weather is currently " + des +"</p>")
+            res.write("<h1>The temperature in kikuyu is " + temp +" degrees Celcius.</h1>")
+            res.write("<img src="+imgURL+">")
+            res.send()
+            
+        });
     });
+
     
 });
 
