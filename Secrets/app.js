84dcf653cd6e75aecd8afd5708e0dbd7973c1ec7
Felix <bosirefelix97@gmail.com>
2023-05-17 00:02:14 +0300

    encryption level 1

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt  = require ("mongoose-encryption");
 
 
 const app = express();
@@ -18,10 +19,13 @@ catch (error) {
     console.log('Error connection: ' + error);
 }
 
-const userSchema={
+const userSchema= new mongoose.Schema({
     email: String,
     password:String
-};
+});
+const secret ="Thisisalittlesecret";
+userSchema.plugin(encrypt,{secret: secret, encryptedFields:["password"]});
+
 const User = new mongoose.model("User", userSchema);
 
 
