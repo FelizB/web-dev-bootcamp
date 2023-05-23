71e0d410d81f0ac2b8d29063ba400a8a94c97621
Felix <bosirefelix97@gmail.com>
2023-05-23 17:00:56 +0300

    use of passport, passport-local-mongoose

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,13 +4,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt  = require ("mongoose-encryption");
+//const encrypt  = require ("mongoose-encryption");
+//const md5 = require("md5")
+//const bcrypt = require("bcrypt")
+const session = require("express-session")
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+app.use(session({
+    secret:process.env.SECRET2,
+    resave: false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 try {
     var db = mongoose.connect('mongodb://127.0.0.1:27017/userDB', {useNewUrlParser: true, dbName: 'userDB' });
@@ -24,10 +37,14 @@ const userSchema= new mongoose.Schema({
     email: String,
     password:String
 });
+//mongoose.set("userCreateIndex", true);
+userSchema.plugin(passportLocalMongoose);
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
 const User = new mongoose.model("User", userSchema);
-
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("home");
@@ -39,6 +56,21 @@ app.route("/login")
     res.render("login");
 })
 .post(function(req,res){
+    const user = new User({
+        username: req.body.username,
+        password:req.body.password
+    });
+    req.login(user, function(err){
+        if (err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res , function(){
+                res.redirect("/secrets")
+            })
+        }
+    })
+})
+/*.post(function(req,res){
     const userN = req.body.username;
     const pass = req.body.password;
     User.findOne({email: userN})
@@ -48,41 +80,78 @@ app.route("/login")
         }
         else{
             if (response){
-                if(response.password === pass){
-                    res.render("secrets")
-                }
-                else{
-                    console.log("No such user was found")
+                bcrypt.compare(pass,response.password,function(err, result){
+                    if(result === true){
+                        res.render("secrets")
+                    }
+                    else{
+                        console.log("No such user was found")
+                    }
+                })
                 }
             }
-        }
     })
     .catch(err=>{
         console.log("Error", err)
     })
-})
+})*/
+
+app.route("/secrets")
+.get(function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    }else{
+        res.redirect("/login")
+    }
+});
+
 
 
 app.route("/register")
 .get(function(req,res){
     res.render("register");
 })
-.post(function(req, res){
-    const newUser = new User ({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save()
-    .then(response =>{
-            console.log("registration was successful", response)
-            res.render("secrets")
+.post(function(req,res){
+    User.register({username:req.body.username}, req.body.password,function(err,user){
+        if (err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets")
+            })
+        }
     })
-    .catch(error =>{
-        console.log("Error", error)
-    });
 })
+/*.post(function(req, res){
 
+    bcrypt.hash(req.body.password, saltRounds,function(err, hash){
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save()
+        .then(response =>{
+                console.log("registration was successful", response)
+                res.render("secrets")
+        })
+        .catch(error =>{
+            console.log("Error", error)
+        });
+    });
+    
+})*/
 
+app.get("/logout", function(req, res){
+    req.logout(function(err, suc){
+        if (err){
+            console.log(err)
+        }else{
+            res.redirect("/");
+        }
+    });
+    
+});
 
 app.listen(3000, function(){
     console.log("The connecton has been established")
